fec373293d883727d8baa266d4247e57528945e7
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-11-24 11:07:16 +0000

    Added the __init__.py script to the settings.py

---
+++ config/settings/__init__.py
@@ -0,0 +1,15 @@
+"""
+Settings module selector based on environment
+"""
+
+import os
+
+# Default to development settings
+environment = os.environ.get('DJANGO_ENVIRONMENT', 'development')
+
+if environment == 'production':
+    from .production import *
+elif environment == 'development':
+    from .development import *
+else:
+    from .base import *
